4e857c40b9c7d095b9c617d62fd74ab898935c9b
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-10-17 21:09:51 +0800

    Use elm-pep only if not supported

--- examples/elm-pep.js
+++ examples/elm-pep.js
@@ -1,58 +1,62 @@
-addMouseToPointerListener( document, 'mousedown', 'pointerdown' )
-addMouseToPointerListener( document, 'mousemove', 'pointermove' )
-addMouseToPointerListener( document, 'mouseup', 'pointerup' )
+if (! ("PointerEvent" in window) ) {
 
-function addMouseToPointerListener( target, mouseType, pointerType ) {
-	target.addEventListener( mouseType, ( mouseEvent ) => {
-		elmPepTarget = findElmPEP( mouseEvent.target )
-		if ( elmPepTarget !== null ) {
-			let pointerEvent = new MouseEvent( pointerType, mouseEvent )
-			pointerEvent.pointerId = 0
-			pointerEvent.isPrimary = true
-			elmPepTarget.dispatchEvent( pointerEvent )
-		}
-	})
-}
+	addMouseToPointerListener( document, 'mousedown', 'pointerdown' )
+	addMouseToPointerListener( document, 'mousemove', 'pointermove' )
+	addMouseToPointerListener( document, 'mouseup', 'pointerup' )
 
-addTouchToPointerListener( document, 'touchstart', 'pointerdown' )
-addTouchToPointerListener( document, 'touchmove', 'pointermove' )
-addTouchToPointerListener( document, 'touchend', 'pointerup' )
-
-function addTouchToPointerListener( target, touchType, pointerType ) {
-	target.addEventListener( touchType, ( touchEvent ) => {
-		elmPepTarget = findElmPEP( touchEvent.target )
-		if ( elmPepTarget !== null ) {
-			let mouseEvent = new CustomEvent( '' )
-			mouseEvent.ctrlKey = touchEvent.ctrlKey
-			mouseEvent.shiftKey = touchEvent.shiftKey
-			mouseEvent.altKey = touchEvent.altKey
-			mouseEvent.metaKey = touchEvent.metaKey
-
-			const changedTouches = touchEvent.changedTouches
-			const nbTouches = changedTouches.length
-			for ( let t=0; t < nbTouches; t++ ) {
-				touch = changedTouches.item( t )
-				mouseEvent.clientX = touch.clientX
-				mouseEvent.clientY = touch.clientY
-				mouseEvent.screenX = touch.screenX
-				mouseEvent.screenY = touch.screenY
-				mouseEvent.pageX = touch.pageX
-				mouseEvent.pageY = touch.pageY
-				rect = touch.target.getBoundingClientRect()
-				mouseEvent.offsetX = touch.clientX - rect.left
-				mouseEvent.offsetY = touch.clientY - rect.top
-
+	function addMouseToPointerListener( target, mouseType, pointerType ) {
+		target.addEventListener( mouseType, ( mouseEvent ) => {
+			elmPepTarget = findElmPEP( mouseEvent.target )
+			if ( elmPepTarget !== null ) {
 				let pointerEvent = new MouseEvent( pointerType, mouseEvent )
-				pointerEvent.pointerId = touch.identifier
-				pointerEvent.isPrimary = (touch.identifier == 0)
+				pointerEvent.pointerId = 0
+				pointerEvent.isPrimary = true
 				elmPepTarget.dispatchEvent( pointerEvent )
 			}
-		}
-	})
-}
+		})
+	}
 
-function findElmPEP ( target ) {
-	if ( document === target ) return null
-	if ( target.hasAttribute( 'elm-pep' ) ) return target
-	return findElmPEP( target.parentNode )
+	addTouchToPointerListener( document, 'touchstart', 'pointerdown' )
+	addTouchToPointerListener( document, 'touchmove', 'pointermove' )
+	addTouchToPointerListener( document, 'touchend', 'pointerup' )
+
+	function addTouchToPointerListener( target, touchType, pointerType ) {
+		target.addEventListener( touchType, ( touchEvent ) => {
+			elmPepTarget = findElmPEP( touchEvent.target )
+			if ( elmPepTarget !== null ) {
+				let mouseEvent = new CustomEvent( '' )
+				mouseEvent.ctrlKey = touchEvent.ctrlKey
+				mouseEvent.shiftKey = touchEvent.shiftKey
+				mouseEvent.altKey = touchEvent.altKey
+				mouseEvent.metaKey = touchEvent.metaKey
+
+				const changedTouches = touchEvent.changedTouches
+				const nbTouches = changedTouches.length
+				for ( let t=0; t < nbTouches; t++ ) {
+					touch = changedTouches.item( t )
+					mouseEvent.clientX = touch.clientX
+					mouseEvent.clientY = touch.clientY
+					mouseEvent.screenX = touch.screenX
+					mouseEvent.screenY = touch.screenY
+					mouseEvent.pageX = touch.pageX
+					mouseEvent.pageY = touch.pageY
+					rect = touch.target.getBoundingClientRect()
+					mouseEvent.offsetX = touch.clientX - rect.left
+					mouseEvent.offsetY = touch.clientY - rect.top
+
+					let pointerEvent = new MouseEvent( pointerType, mouseEvent )
+					pointerEvent.pointerId = touch.identifier
+					pointerEvent.isPrimary = (touch.identifier == 0)
+					elmPepTarget.dispatchEvent( pointerEvent )
+				}
+			}
+		})
+	}
+
+	function findElmPEP ( target ) {
+		if ( document === target ) return null
+		if ( target.hasAttribute( 'elm-pep' ) ) return target
+		return findElmPEP( target.parentNode )
+	}
+
 }
